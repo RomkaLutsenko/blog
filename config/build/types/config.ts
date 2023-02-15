0f28817110f20aa56857d1110ca9b48bdbb34d7c
Roman <RomanLutsenko2001@gmail.com>
2023-02-15 22:56:22 +0700

    like a first commit

---
+++ config/build/types/config.ts
@@ -0,0 +1,19 @@
+export type mode = 'development' | 'production'
+
+export interface BuildPaths {
+    entry: string;
+    build: string;
+    html: string;
+}
+
+export interface BuildEnv {
+    mode: mode;
+    port: number;
+}
+
+export interface BuildOptions {
+    mode: mode;
+    paths: BuildPaths;
+    isDev: boolean;
+    port: number;
+}
